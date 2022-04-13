15683d6054826ed5a7b5ade8dfc497b211bd5d0c
Sydney <sydneysirois17@gmail.com>
2022-04-13 19:07:44 -0400

    variables set up

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const validatePassword= (password) => {
-  
+  let hasUpperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let hasLowerCase = 'abcdefghijklmnopqrstuvwxyz'
+  let hasNumber = '0123456789'
+  let hasSpecial = '!@#$%^&*()-_=+'
+
 } 
 
 module.exports = validatePassword
