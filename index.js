61cf65c19d03aa479514d4a391ccb877752db820
Sydney <sydneysirois17@gmail.com>
2022-04-17 13:21:30 -0400

    all functions submitted, 4/5 tests pass

--- index.js
+++ index.js
@@ -1,31 +1,77 @@
-const validatePassword= (password) => {
+const checkUpperCase = (letter) => {
   let hasUpperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+  for (let i = 0; i < hasUpperCase.length; i++) {
+    const currentItem = hasUpperCase[i]
+
+    if (currentItem === letter) return true
+  }
+
+  return false
+}
+
+const checkLowerCase = (letter) => {
   let hasLowerCase = 'abcdefghijklmnopqrstuvwxyz'
-  let hasNumber = '0123456789'
-  let hasSpecial = '!@#$%^&*()-_=+'
+
+  for (let i = 0; i < hasLowerCase.length; i++) {
+    const currentItem = hasLowerCase[i]
+
+    if (currentItem === letter) return true
+  }
+
+  return false
+}
+
+const checkNumbers = (letter) => {
+  let hasNumber = '1234567890'
+
+  for (let i = 0; i < hasNumber.length; i++) {
+    const currentItem = hasNumber[i]
+
+    if (currentItem === letter) return true
+  }
+
+  return false
+}
+
+const checkSpecCharacter = (letter) => {
+  let hasSpecial = '1234567890'
+
+  for (let i = 0; i < hasSpecial.length; i++) {
+    const currentItem = hasSpecial[i]
+
+    if (currentItem === letter) return true
+  }
+
+  return false
+}
 
 
-    for(let i = 0; i < password.length; i++){
-        if(checkUpperCase(password[i])=== true) {
-            hasUpperCase = true
-        }
+const validatePassword = (password) => {
+  let hasLowerCase = false
+  let hasNumber = false
+  let hasSpecial = false
+  let hasUpperCase = false
 
-        if(checkLowerCase(password[i])=== true) {
-            hasLowerCase = true
-        }
-
-        if(checkNumbers(password[i])=== true) {
-            hasNumber = true
-        }
-
-        if(checkSpecCharacter(password[i])=== true) {
-            hasSpecial = true
-        }
-
+  for (let i = 0; i < password.length; i++) {
+    if (checkUpperCase(password[i]) === true) {
+      hasUpperCase = true
     }
-    
-    return (hasUpperCase && hasLowerCase && hasNumber && hasSpecial && password.length >= 8)
 
+    if (checkLowerCase(password[i]) === true) {
+      hasLowerCase = true
+    }
+
+    if (checkNumbers(password[i]) === true) {
+      hasNumber = true
+    }
+
+    if (checkSpecCharacter(password[i]) === true) {
+      hasSpecial = true
+    }
+  }
+
+  return (hasUpperCase && hasLowerCase && hasNumber && hasSpecial && password.length >= 8)
 }
 
 module.exports = validatePassword
